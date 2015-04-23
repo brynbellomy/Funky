5d30b2ecda85ce5ee822e850741b996bff507c54
bryn austin bellomy <bryn.bellomy@gmail.com>
2015-04-23 04:21:19 -0500

    regenerating pods, adding functions.result.swift

---
+++ Pods/Target Support Files/Pods-FunkyTests/Pods-FunkyTests-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// LlamaKit
+#define COCOAPODS_POD_AVAILABLE_LlamaKit
+#define COCOAPODS_VERSION_MAJOR_LlamaKit 0
+#define COCOAPODS_VERSION_MINOR_LlamaKit 6
+#define COCOAPODS_VERSION_PATCH_LlamaKit 0
+
+// Nimble
+#define COCOAPODS_POD_AVAILABLE_Nimble
+#define COCOAPODS_VERSION_MAJOR_Nimble 0
+#define COCOAPODS_VERSION_MINOR_Nimble 4
+#define COCOAPODS_VERSION_PATCH_Nimble 2
+
+// Quick
+#define COCOAPODS_POD_AVAILABLE_Quick
+#define COCOAPODS_VERSION_MAJOR_Quick 0
+#define COCOAPODS_VERSION_MINOR_Quick 3
+#define COCOAPODS_VERSION_PATCH_Quick 1
+
+// Regex
+#define COCOAPODS_POD_AVAILABLE_Regex
+#define COCOAPODS_VERSION_MAJOR_Regex 0
+#define COCOAPODS_VERSION_MINOR_Regex 1
+#define COCOAPODS_VERSION_PATCH_Regex 0
+
